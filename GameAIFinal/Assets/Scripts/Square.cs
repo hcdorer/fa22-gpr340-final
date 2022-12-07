a3843724c30eae64d416c91ed54a7541dbb3f59b
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-12-07 13:50:41 -0500

    Set up maze, recreating original as closely as possible

--- GameAIFinal/Assets/Scripts/Square.cs
+++ GameAIFinal/Assets/Scripts/Square.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class Square : GridAligned {
     [SerializeField] SquareSprites sprites;
 
-    [SerializeField] bool north, east, south, west;
+    [SerializeField] bool northWall = true, eastWall = true, southWall = true, westWall = true;
+    private Square northNeighbor { get => getSquareAt(levelGrid.CellToWorld(new Vector3Int(gridPosition.x, gridPosition.y + 1, 0))); }
+    private Square eastNeighbor { get => getSquareAt(levelGrid.CellToWorld(new Vector3Int(gridPosition.x + 1, gridPosition.y, 0))); }
+    private Square southNeighbor { get => getSquareAt(levelGrid.CellToWorld(new Vector3Int(gridPosition.x, gridPosition.y - 1, 0))); }
+    private Square westNeighbor { get => getSquareAt(levelGrid.CellToWorld(new Vector3Int(gridPosition.x - 1, gridPosition.y, 0))); }
 
     private void OnValidate() {
         setSprite();
+        setNeighborWalls();
     }
 
     private void setSprite() {
-        string spriteName = "Square" + (north ? "N" : "") + (east ? "E" : "") + (south ? "S" : "") + (west ? "W" : "");
+        string spriteName = "Square" + (northWall ? "N" : "") + (eastWall ? "E" : "") + (southWall ? "S" : "") + (westWall ? "W" : "");
         GetComponent<SpriteRenderer>().sprite = sprites.getSpriteByName(spriteName);
+    }
+
+    private void setNeighborWalls() {
+        if(northNeighbor != null) {
+            northNeighbor.southWall = northWall;
+            northNeighbor.setSprite();
+        }
+
+        if(eastNeighbor != null) {
+            eastNeighbor.westWall = eastWall;
+            eastNeighbor.setSprite();
+        }
+
+        if(southNeighbor != null) {
+            southNeighbor.northWall = southWall;
+            southNeighbor.setSprite();
+        }
+
+        if(westNeighbor != null) {
+            westNeighbor.eastWall = westWall;
+            westNeighbor.setSprite();
+        }
+    }
+
+    private static Square getSquareAt(Vector3 position) {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, 0.1f);
+        foreach(Collider2D c in colliders) {
+            if(c.gameObject.tag == "Square") {
+                return c.GetComponent<Square>();
+            }
+        }
+
+        return null;
     }
 }
