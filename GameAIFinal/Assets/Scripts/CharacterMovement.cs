447fc9b05b51654e00ea76a0478cef9f94e1d51d
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-12-09 01:17:51 -0500

    Finished writing CharacterMovement script

--- GameAIFinal/Assets/Scripts/CharacterMovement.cs
+++ GameAIFinal/Assets/Scripts/CharacterMovement.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour 
+public class CharacterMovement : GridAligned 
 {
-    private Vector2 direction;
-    private Vector2 targetGridPosition;
+    private Vector2Int direction = -Vector2Int.right;
+    private Vector2Int Direction { set => direction = value; }
+    private Vector2Int targetGridPosition;
+    [SerializeField] private float moveSpeed;
 
-    [SerializeField] private Grid levelGrid;
+    private void Start() {
+        targetGridPosition = gridPosition + direction;
+    }
 
-    private void lerp()
-    {
-        transform.position = Vector2.MoveTowards(transform.position)
+    private void Update() {
+        if(lerp()) {
+            gridPosition = targetGridPosition;
+            targetGridPosition += direction;
+        }
+    }
+
+    private bool lerp() {
+        Vector3 targetPosition = levelGrid.CellToWorld(new Vector3Int(targetGridPosition.x, targetGridPosition.y, 0));
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        if(transform.position == targetPosition) {
+            return true;
+        }
+        return false;
     }
 }
